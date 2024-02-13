2632c8677c3c46f088348e4af63eaf88fcae8659
Pasha <pavelzobov123@gmail.com>
2024-02-12 21:30:37 -0500

    Improved function, removed all unnecessary code, provided documentation

--- graph_generator.py
+++ graph_generator.py
@@ -1,16 +1,35 @@
-
 import json
-import plotly.utils as putil
 import plotly.express as px
+import plotly.utils as putil
 
 
 def generate_graph(reviews_dataframe, graph_title):
+    """
+    Generates a bar graph of review scores from a DataFrame.
+
+    Parameters:
+    - reviews_dataframe: A pandas DataFrame containing a 'Score' column, or None.
+    - graph_title: A string specifying the title of the graph.
+
+    Returns:
+    - A JSON string representing the generated graph, or an empty JSON object if the input is None or empty.
+    """
+
+    # Proceed if the DataFrame is valid but check if it's empty
+    if reviews_dataframe.empty:
+        return json.dumps({})
+
+    # Aggregate review scores and prepare the data for plotting
     scores_dataframe = (reviews_dataframe['Score']
                         .value_counts()
                         .reset_index(name='counts')
                         .rename(columns={'index': 'Review Score', 'counts': 'Score Total'})
-                        .sort_values(by=['Review Score'], ascending=False))
-    print(scores_dataframe)
+                        .sort_values(by=['Review Score'], ascending=True))
+
+    # Generate the bar graph using Plotly Express
     graph = px.bar(scores_dataframe, x='Review Score', y='Score Total', barmode='group', title=graph_title)
+
+    # Convert the graph to JSON using Plotly's JSON encoder
     graph_json = json.dumps(graph, cls=putil.PlotlyJSONEncoder)
+
     return graph_json
