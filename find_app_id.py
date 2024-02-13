eea6cfe2feceec796c30f88b854bca82a5640b61
Pasha <pavelzobov123@gmail.com>
2024-02-12 22:35:37 -0500

    Minor cosmetic improvements

--- find_app_id.py
+++ find_app_id.py
@@ -37,7 +37,7 @@ class AppFinder:
 
         self.google_id = ""
         self.apple_id = ""
-        self.google_search_api_key = os.getenv('GOOGLE_SEARCH_API_KEY_PASHA')
+        self.google_search_api_key = os.getenv('GOOGLE_SEARCH_API_KEY')
         self.engine = "c5a5f09a33422445a"
 
     def find_app(self, app_name):
