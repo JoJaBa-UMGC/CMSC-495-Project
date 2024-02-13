2a6f9bec6e1ea5ee37abb6722b8289c0978d7408
Pasha <pavelzobov123@gmail.com>
2024-02-12 21:32:45 -0500

    Improved functions, restructured, removed all unnecessary code and repetitions, provided documentation

--- main.py
+++ main.py
@@ -1,81 +1,68 @@
 import os
-
-import play_store_reviews
+from flask import Flask, render_template, request, session
 import app_store_reviews
+import play_store_reviews
 import graph_generator
 import csv_generator
 from find_app_id import AppFinder
 
-from flask import Flask, render_template, request
-
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.secret_key = os.urandom(24)
 
-df_google_reviews = None
-df_appsto_reviews = None
+
+def get_reviews_for_platform(days, platform):
+    """Fetch reviews for a given platform and return HTML or an empty list."""
+    if platform == 'Google':
+        df_reviews = play_store_reviews.get_reviews(days, session.get('google_id'))
+    else:
+        df_reviews = app_store_reviews.get_reviews(days, session.get('apple_id'))
+
+    graph = graph_generator.generate_graph(df_reviews, platform + " Reviews Scores")
+
+    return df_reviews, graph
+
+
+def reviews_to_html(reviews):
+    if reviews is not None and len(reviews) > 0:
+        reviews.index = range(1, len(reviews) + 1)
+        return [reviews.to_html(classes="data", header=True)]
+
 
 @app.route('/', methods=['POST', 'GET'])
 def landing_page():
     if request.method == 'POST':
         app_name = request.form.get('app_name')
-        search_period = request.form.get('search_period')
-        global df_google_reviews
-        global df_appsto_reviews
+        search_period = request.form.get('search_period', 'month')
         return show_forum_report_page(app_name, search_period)
-
-    return show_landing_page()
-
-
-def show_landing_page():
     return render_template('landing.html')
 
 
 def show_forum_report_page(app_name, search_period):
-    global df_google_reviews
-    global df_appsto_reviews
+    global google_reviews, appstore_reviews
+    search_periods = {'month': 30, 'two-months': 60, 'quarter': 91}
+    days = search_periods.get(search_period, 30)
+
     app_search = AppFinder()
     app_search.find_app(app_name)
 
-    days = {
-        'month': 30,
-        'quarter': 91,
-        'half-year': 182,
-        'year': 365,
-        'whole-period': -1
-    }[search_period]
-
-    df_google_reviews = play_store_reviews.get_reviews(days, app_search.google_id)
-
-    df_appsto_reviews = app_store_reviews.get_reviews(days, app_search.apple_id)
-
-    if len(df_google_reviews) > 0:
-        google_reviews = [df_google_reviews.to_html(classes="data", header=True)]
-    else:
-        google_reviews = []
-
-    if len(df_appsto_reviews) > 0:
-        appstore_reviews = [df_appsto_reviews.to_html(classes="data", header=True)]
-    else:
-        appstore_reviews = []
-
-    google_graph = graph_generator.generate_graph(df_google_reviews, 'Google Review Scores')
-    apple_graph = graph_generator.generate_graph(df_appsto_reviews, 'Apple Review Scores')
+    google_reviews = get_reviews_for_platform(days, 'Google')
+    appstore_reviews = get_reviews_for_platform(days, 'Apple')
 
     return render_template('review_display.html',
-                           google_reviews=google_reviews,
-                           appstore_reviews=appstore_reviews,
-                           google_graph_json=google_graph,
-                           apple_graph_json=apple_graph)
+                           google_reviews=reviews_to_html(google_reviews[0]),
+                           appstore_reviews=reviews_to_html(appstore_reviews[0]),
+                           google_graph_json=google_reviews[1],
+                           apple_graph_json=appstore_reviews[1])
 
 
-@app.route("/google")
+@app.route('/google')
 def google_csv():
-    return csv_generator.generate_csv_zip(df_google_reviews, "Google Reviews")
+    return csv_generator.generate_csv(google_reviews[0], "Google")
 
 
-@app.route("/apple")
+@app.route('/apple')
 def apple_csv():
-    return csv_generator.generate_csv_zip(df_appsto_reviews, "Apple Reviews")
+    return csv_generator.generate_csv(appstore_reviews[0], "Apple")
 
 
 if __name__ == '__main__':
